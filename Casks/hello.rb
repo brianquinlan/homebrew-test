490daf0b21662c0eaa858f98258253b6b701a4af
Brian Quinlan <bquinlan@google.com>
2023-12-26 10:29:39 -0800

    Create hello.rb

---
+++ Casks/hello.rb
@@ -0,0 +1,16 @@
+cask "hello" do
+  version "0.0.1"
+  sha256 "bc3ec57aa8c5c35bdbd8f10a1474f79aff277bf128069bf36da4eb9487c0a809"
+
+  url "https://storage.googleapis.com/dart-publish-test/hello-#{version}.zip",
+      verified: "storage.googleapis.com/dart-publish-test/"
+  name "hello"
+  desc "Turns your Trello cards into Pomodoro tasks"
+  homepage "https://pomelloapp.com/"
+
+  binary "hello"
+end
+
+# brew install --cask https://raw.githubusercontent.com/brianquinlan/homebrew-test/main/hello.rb
+# binary "hello"
+# shasum -a 256 hello-0.0.1.zip
