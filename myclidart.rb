bb550ab5bf1d215b50067330d311ad934bf5d3de
Release Server <noreply@release.org>
2023-12-26 18:02:54 -0800

    Updated myclidart to 1.0.0

--- myclidart.rb
+++ myclidart.rb
@@ -1,6 +1,6 @@
 cask "myclidart" do
   version "1.0.0"
-  sha256 "afab018fcf31e99739fa1b9da5be639ab75424ec378a0634414db11a8c0ad3eb"
+  sha256 "309def119ccf34515891ad3558e63eaca9de8d21ab1f1923b70dacc981080136"
 
   url "https://storage.googleapis.com/dart-publish-test/myclidart-#{version}.zip",
       verified: "storage.googleapis.com/dart-publish-test/"
