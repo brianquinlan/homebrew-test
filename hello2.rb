b74d860da86e0b18bd3fc7d21a5a7733282315c6
Release Server <noreply@release.org>
2023-12-26 17:40:56 -0800

    Updated hello2 to 1.0.2

--- hello2.rb
+++ hello2.rb
@@ -1,10 +1,10 @@
 cask "hello2" do
   version "1.0.2"
-  sha256 "18a8d7b4193f0abce18d813b5cc8d0a02e4be02cd947171ed9c83926f3427845"
+  sha256 "827a42cc43c4e7d19fa566d337ed3f26080993f23ad7453e923b2efd877bfc39"
 
   url "https://storage.googleapis.com/dart-publish-test/hello2-#{version}.zip",
       verified: "storage.googleapis.com/dart-publish-test/"
-  name "hello"
+  name "hello2"
   desc "Turns your Trello cards into Pomodoro tasks"
   homepage "https://pomelloapp.com/"
 
