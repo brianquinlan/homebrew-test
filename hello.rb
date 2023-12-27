a187437934a075f72e868aba00e426dfc7a8bd92
Release Server <noreply@release.org>
2023-12-26 16:54:04 -0800

    Updated hello to 1.0.1

--- hello.rb
+++ hello.rb
@@ -1,8 +1,8 @@
 cask "hello" do
-  version "0.0.1"
-  sha256 "bc3ec57aa8c5c35bdbd8f10a1474f79aff277bf128069bf36da4eb9487c0a809"
+  version "1.0.1"
+  sha256 "328c82a4fb8cdf13e9638e54572255be3e346939b85cba07043b44bc3e0e8865"
 
-  url "https://storage.googleapis.com/dart-publish-test/hello-#{version}.zip",
+  url "https://storage.googleapis.com/dart-publish-test/hello-#{1.0.1}.zip",
       verified: "storage.googleapis.com/dart-publish-test/"
   name "hello"
   desc "Turns your Trello cards into Pomodoro tasks"
@@ -10,8 +10,3 @@ cask "hello" do
 
   binary "hello"
 end
-
-# brew install --cask https://raw.githubusercontent.com/brianquinlan/homebrew-test/main/hello.rb
-# brew install brianquinlan/homebrew-test/hello
-# binary "hello"
-# shasum -a 256 hello-0.0.1.zip
