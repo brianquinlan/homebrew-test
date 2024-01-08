5addf3d3111ea3e133e5c181a276e587396ed52d
Release Server <noreply@release.org>
2024-01-08 15:28:13 -0800

    Updated foo to 1.0.0

--- foo.rb
+++ foo.rb
@@ -1,6 +1,6 @@
 cask "foo" do
   version "1.0.0"
-  sha256 "08c85305a457932c9b0e0c31d13f8e9156458e639fafc39f2c9c1fef554b95b5"
+  sha256 "eb43d0937011172a43a926d33037ada95a14bd8ba8ab4cd1620c9b0e8c620f46"
 
   url "https://storage.googleapis.com/dart-publish-test/foo-#{version}.zip",
       verified: "storage.googleapis.com/dart-publish-test/"
